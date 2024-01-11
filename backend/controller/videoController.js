59a680e7ce31650db3ff1a1a0beac2be357b72f1
Anshul45 <anshulgarwal025@gmail.com>
2024-01-11 22:29:32 +0530

    add cloudinary to project

--- backend/controller/videoController.js
+++ backend/controller/videoController.js
@@ -1,16 +1,23 @@
-import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+});
+
 export const uploadVideo = async (req, res) => {
   try {
     const { subtitle } = req.body;
     const videoFile = req.file;
 
-    const videoUrl = `data:video/mp4;base64,${videoFile.buffer.toString(
-      "base64"
-    )}`;
-
-    res.status(200).json({
-      videoUrl,
+    const uploadResult = await cloudinary.uploader.upload(videoFile.path, {
+      folder: "rapidquest",
+      resource_type: "video",
     });
+
+    res.status(200).json({ result: uploadResult });
   } catch (error) {
     console.error("Error uploading video:", error);
     res.status(500).json({ error: "Internal Server Error" });
